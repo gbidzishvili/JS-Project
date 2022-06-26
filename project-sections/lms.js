c1340b1124f5fc580e4b189df589f81c99417db0
gbidzishvili <bidzishviligiorgi7@gmail.com>
2022-06-26 22:29:05 +0400

    lms.js is done

--- project-sections/lms.js
+++ project-sections/lms.js
@@ -1,3 +1,4 @@
+const map = new Map();
 class Subject {
   constructor({ title, lessons, description }) {
     if (typeof title !== "string")
@@ -17,6 +18,34 @@ const history = new Subject({
   lessons: 24,
   description: "",
 });
-console.log(history.title);
-console.log(history.lessons);
-console.log(history.id);
+// console.log(history);
+// console.log(history.lessons);
+// console.log(history.id);
+
+class LMS {
+  add(subject) {
+    map.set(subject.id, subject);
+  }
+  remove(subject) {
+    map.delete(subject.id);
+  }
+  verify(subject) {
+    return map.has(subject.id);
+  }
+  readAll(und) {
+    if (und !== undefined) throw new Error("readAll must be empty");
+    const subjectsArray = [];
+    map.forEach((value, key, ownMap) => {
+      // console.log(key, value);
+      subjectsArray.push(value);
+    });
+    console.log(subjectsArray);
+    return subjectsArray;
+  }
+}
+
+const lms = new LMS();
+lms.add(history); // should add subject to lms
+// lms.remove(history); // should remove subject from lms
+// console.log(lms.verify(history));
+// lms.readAll();
