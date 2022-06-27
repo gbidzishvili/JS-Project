3797dd2fbad7e06d3fcbf6e68ec1627449fd6733
gbidzishvili <bidzishviligiorgi7@gmail.com>
2022-06-28 02:35:45 +0400

    Teachers modified

--- project-sections/Teachers.mjs
+++ project-sections/Teachers.mjs
@@ -6,6 +6,7 @@ import {
   validateSex,
   validateSubject,
   validatedescription,
+  validateId,
 } from "./validations.mjs";
 let map = new Map();
 export class Teachers {
@@ -23,10 +24,24 @@ export class Teachers {
     return teacherId;
   }
   read(teacherId) {
+    validateId(teacherId);
     // console.log(map.get(teacherId));
     return map.get(teacherId);
   }
   update(teacherId, updatedProfile) {
+    validateId(teacherId);
+    if (updatedProfile.name !== undefined) validateName(updatedProfile.name);
+    if (updatedProfile.dateOfBirth !== undefined)
+      validateDateOfBirth(updatedProfile.dateOfBirth);
+    if (updatedProfile.emails !== undefined)
+      validateEmails(updatedProfile.emails);
+    if (updatedProfile.phones !== undefined)
+      validatePhones(updatedProfile.phones);
+    if (updatedProfile.sex !== undefined) validateSex(updatedProfile.sex);
+    if (updatedProfile.subjects !== undefined)
+      validateSubject(updatedProfile.subjects);
+    if (updatedProfile.description !== undefined)
+      validatedescription(updatedProfile.description);
     const old = { ...map.get(teacherId) };
     const spreaded = { ...updatedProfile };
     const updated = { ...old, ...spreaded };
@@ -35,6 +50,7 @@ export class Teachers {
     return updated;
   }
   remove(teacherId) {
+    validateId(teacherId);
     map.delete(teacherId);
   }
 }
