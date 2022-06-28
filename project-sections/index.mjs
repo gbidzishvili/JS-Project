28dad8f9edba3e6d33b8b6eb5d3040aa3e714e7f
gbidzishvili <bidzishviligiorgi7@gmail.com>
2022-06-28 17:01:21 +0400

    index added

--- project-sections/index.mjs
+++ project-sections/index.mjs
@@ -2,28 +2,29 @@ import { Subject, LMS } from "./lms.mjs";
 import { Teachers } from "./Teachers.mjs";
 import { Pupils } from "./Pupils.mjs";
 import { Groups } from "./Groups.mjs";
-const history = new Subject({
-  title: "History",
-  lessons: 24,
-  // description: "history is savage",
-});
+import { Gradebooks } from "./Gradebooks.mjs";
 const Biology = new Subject({
   title: "Biology",
+  lessons: 20,
+  // description: "Biology",
+});
+const history = new Subject({
+  title: "History",
   lessons: 24,
   description: "history is savage",
 });
 // console.log(history);
 // console.log(history.lessons);
 // console.log(history.id);
-// const lms = new LMS();
-// lms.add(history); // should add subject to lms
-// lms.add(Biology); // should add subject to lms
+const lms = new LMS();
+lms.add(history); // should add subject to lms
+lms.add(Biology); // should add subject to lms
 // lms.remove(history); // should remove subject from lms
 // console.log(lms.verify(history));
 // lms.readAll();
 // /////////////////////////////////////////////////////
 let teacher1 = {
-  name: { first: "giorgi", last: "bidz" },
+  name: { first: "Elizabeth", last: "Holms" },
   dateOfBirth: "07-22-2000",
   emails: [
     {
@@ -31,7 +32,7 @@ let teacher1 = {
       primary: true,
     },
     {
-      email: "giorgi@example.com",
+      email: "Elizabeth@example.com",
       primary: false,
     },
   ],
@@ -45,7 +46,7 @@ let teacher1 = {
       primary: false,
     },
   ],
-  sex: "male", // male or female
+  sex: "female", // male or female
   subjects: [
     {
       subject: "Biology", // just name property of subject.
@@ -89,7 +90,7 @@ let teacher2 = {
 };
 const teachers = new Teachers();
 const teacherId = teachers.add(teacher1);
-// const teacher2Id = teachers.add(teacher2);
+const teacher2Id = teachers.add(teacher2);
 // console.log(teachers.read(teacherId));
 // teachers.remove(teacherId);
 // console.log(teachers.read(teacherId));
@@ -119,7 +120,7 @@ let pupil1 = {
   sex: "male", // male or female
 };
 let pupil2 = {
-  name: { first: "mariam", last: "vashakhmadze" },
+  name: { first: "Oliver", last: "Black" },
   dateOfBirth: "11-31-1975",
   phones: [
     {
@@ -131,7 +132,7 @@ let pupil2 = {
       primary: false,
     },
   ],
-  sex: "female", // male or female
+  sex: "male", // male or female
 };
 const pupils = new Pupils();
 const pupil = pupils.add(pupil1);
@@ -153,12 +154,12 @@ const pupil_2 = pupils.add(pupil2);
 const room = 236;
 const groups = new Groups();
 // Create a new group. add methods takes integer as a parameter. returns id of group
-// const groupId = groups.add(room);
+const groupId = groups.add(room);
 // const groupId2 = groups.add(700);
-// // console.log(groupId);
+// console.log(groupId);
 // // Add this pupil to this group
-// groups.addPupil(groupId, pupil1);
-// groups.addPupil(groupId, pupil2);
+groups.addPupil(groupId, pupil1);
+groups.addPupil(groupId, pupil2);
 // // Remove this pupil from this group
 // groups.removePupil(groupId, pupil1.id);
 // // Update room for this group
@@ -175,4 +176,43 @@ const groups = new Groups();
 // // }
 
 // // It will return array of groups
-// groups.readAll();
+
+// console.log(groups.readAll()[0].pupils);
+// ///////////////////////////////////////////////////////
+const pupilId = pupil2.id;
+// console.log(pupilId,);
+const teacherId1 = teacherId;
+// console.log(teacherId1);
+const gradebooks = new Gradebooks(groups, teachers, lms);
+// Create a new gradebook.
+const gradebook = gradebooks.add(groupId);
+let gradebookId = groupId;
+// console.log(groupId);
+// // Destroy all data inside this gradebook
+// gradebooks.clear();
+
+// // shceme of a record. all fields are required.
+const record = {
+  pupilId: pupilId,
+  teacherId: teacherId,
+  subjectId: history.id,
+  lesson: 1,
+  mark: 9,
+};
+gradebooks.addRecord(gradebookId, record);
+// // Read information about oliver results
+// const oliver = gradebooks.read(gradebookId, pupilId);
+// {
+//   name: 'Oliver Black',
+//   records: [
+//     {
+//       teacher: 'Elizabeth Holms',
+//       subject: 'History',
+//       lesson: 1,
+//       mark: 9
+//     }
+//   ]
+// }
+
+// // Read information about all students in this gradebook
+const students = gradebooks.readAll(gradebookId); // It will return the array of objects
