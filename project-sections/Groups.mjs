98b6cbcd817909a54e9c49fa74c9cc07b5e8c126
gbidzishvili <bidzishviligiorgi7@gmail.com>
2022-06-28 17:58:16 +0400

    Groups file is done

--- project-sections/Groups.mjs
+++ project-sections/Groups.mjs
@@ -1,52 +1,52 @@
 import { validatePupil, validateId, validateRoom } from "./validations.mjs";
 const room = 236;
-const map = new Map();
 export class Groups {
   constructor() {
+    const map = new Map();
     this.map = map;
   }
   add(room) {
     validateRoom(room);
     const groupId = Math.random().toString().slice(2);
-    map.set(groupId, { id: groupId, room, pupils: [] });
+    this.map.set(groupId, { id: groupId, room, pupils: [] });
     return groupId;
   }
   addPupil(groupId, pupil) {
     validatePupil(pupil);
     validateId(groupId);
-    map.get(groupId).pupils.push(pupil);
+    this.map.get(groupId).pupils.push(pupil);
   }
   removePupil(groupId, pupilId) {
     validateId(groupId);
     validateId(pupilId);
     const remains = [];
-    map.get(groupId).pupils.forEach((value, key, obj) => {
+    this.map.get(groupId).pupils.forEach((value, key, obj) => {
       if (value.id !== pupilId) {
         remains.push(value);
       }
     });
-    map.get(groupId).pupils = remains;
+    this.map.get(groupId).pupils = remains;
   }
   update(groupId, obj) {
     validateId(groupId);
     if (obj.room !== undefined) validateRoom(obj.room);
     validateRoom(obj.room);
-    const old = { ...map.get(groupId) };
+    const old = { ...this.map.get(groupId) };
     const spreaded = { ...obj };
     const updated = { ...old, ...spreaded };
-    map.set(groupId, updated);
+    this.map.set(groupId, updated);
     console.log(updated);
     return updated;
   }
   read(groupId) {
     validateId(groupId);
-    // console.log(map.get(groupId));
-    map.get(groupId);
+    // console.log(this.map.get(groupId));
+    this.map.get(groupId);
   }
   readAll(und) {
     if (und !== undefined) throw new Error("readAll must be empty");
     const arr = [];
-    map.forEach((value, key, obj) => {
+    this.map.forEach((value, key, obj) => {
       arr.push(value);
     });
     // console.log(arr);
